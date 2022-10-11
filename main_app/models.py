91d77eb77b511a9f96b36732b8e065ed69bc4bca
RickHayden <richard.hayden459@live.com>
2022-10-11 10:36:44 -0400

    merch model established

--- main_app/models.py
+++ main_app/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
 # Create your models here.
+
+class Merch(models.Model):
+
+    name = models.CharField(max_length=300)
+    img = models.CharField(max_length=450)
+    bio = models.TextField(max_length=800)
+    created_at = models.DateTimeField(auto_now_add=True)
+    
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        ordering = ['name']
